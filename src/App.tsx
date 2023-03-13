19d2b695ff050e18b8661f83f70c171281f517fc
FlorianKaulfersch <florian.kaulfersch@netlight.com>
2023-03-13 15:42:33 +0100

    added a possible solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Elevator, ElevatorControls } from "./Elevator";
 
+/**
+ * Waits for the specified amount of milliseconds
+ * @param ms
+ */
+const waitFor = (ms: number) => new Promise<void>((resolve) => setTimeout(() => resolve(),ms))
+
 function App() {
   const [queue, setQueue] = useState<number[]>([]);
   const [currentLevel, setCurrentLevel] = useState(0);
   const elevatorRef = useRef<HTMLDivElement>(null);
+  const [open, setOpen] = useState(false)
 
   const addToButtonQueue = (newLevel: number) => {
     //
     // Here you have to create a new queue that also includes the given newLevel,
     // and set your queue to the new value
+    setQueue(oldQueue => [...oldQueue, newLevel])
   };
 
   useEffect(() => {
     // This fires whenever our queue changes
 
     // First we need a check: When our queue is empty, we don't want anything to happen
-    const queueIsEmpty = false;
+    const queueIsEmpty = queue.length === 0;
     if (queueIsEmpty) return;
     // If the queue is not empty (if something has been added to it for example)
     // We want the elevator to move to the next destination after a set amount of time
@@ -25,6 +33,7 @@ function App() {
     const timeOut = setTimeout(() => {
       // Here, we want to set the current level to an element in the queue (but dont't remove it from the queue yet)
       // The removing will be done when the elevator reaches the floor
+      setCurrentLevel(queue[0])
     }, timeUntilTheElevatorMovesInMS);
     return () => {
       clearTimeout(timeOut);
@@ -35,9 +44,14 @@ function App() {
     const currentElevatorRef = elevatorRef.current;
     // This function is put into a variable here so that we can remove the function reference in the cleanup function
     // returned from the useEffect
-    const onReachFloor = () => {
+    const onReachFloor = async () => {
       // When we reach the destination floor, we want to remove that floor from our queue
-      console.log(`Reached floor ${currentLevel}`);
+      await waitFor(500)
+      setOpen(true);
+      await waitFor(2000)
+      setOpen(false)
+      await waitFor(500)
+      setQueue(([_, ...rest]) => rest)
     };
 
     currentElevatorRef?.addEventListener("transitionend", onReachFloor);
@@ -48,7 +62,7 @@ function App() {
 
   return (
     <div className="App">
-      <Elevator ref={elevatorRef} level={currentLevel} />
+      <Elevator ref={elevatorRef} level={currentLevel} open={open} />
       <ElevatorControls
         currentLevel={currentLevel}
         queue={queue}
